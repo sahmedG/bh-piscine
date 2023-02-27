004d338dcfdd8ab34a12d6927d0509b0bb0fb5bc
Sameer Ahmed <sahmed@noreply.learn.reboot01.com>
2023-02-27 10:48:18 +0000

    Update 'myfamily.sh'

--- myfamily.sh
+++ myfamily.sh
@@ -1,2 +1,2 @@
-export HERO_ID=2
+export HERO_ID=$HERO_ID
 curl -s https://learn.reboot01.com/assets/superhero/all.json | jq --arg id "$HERO_ID" ".[] | select(.id==$HERO_ID) | .connections.relatives" | tr -d '"'
