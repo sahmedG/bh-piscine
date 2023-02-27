3c4cf03450f82ed6e332bcc0ed41b478519daf3b
Sameer Ahmed <sahmed@noreply.learn.reboot01.com>
2023-02-27 09:43:25 +0000

    Update 'myfamily.sh'

--- myfamily.sh
+++ myfamily.sh
@@ -1,1 +1,2 @@
-curl -s https://learn.reboot01.com/assets/superhero/all.json | jq ' .[] | select(.id == 1) | .relatives '
+#curl -s https://learn.reboot01.com/assets/superhero/all.json | jq ' .[] | select(.id == 1) | .relatives '
+curl -s https://learn.reboot01.com/assets/superhero/all.json | jq '.[] | select(.id == 1) | .relatives'
