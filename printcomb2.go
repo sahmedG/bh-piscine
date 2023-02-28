fb9febb2e46b22121a83f20ea0fb550937213d4f
reboot <sahmed>
2023-02-28 21:37:46 +0300

    second comb

--- printcomb2.go
+++ printcomb2.go
@@ -7,13 +7,19 @@ func PrintComb2() {
 		for j := '0'; j <= '8'; j++ {
 			for k := '0'; k <= '9'; k++ {
 				for y := '0'; y <= '9'; y++ {
+					if y == j {
+						continue
+					}
+					if y == '0' {
+						continue
+					}
 					z01.PrintRune(i)
 					z01.PrintRune(j)
 					z01.PrintRune(' ')
 					z01.PrintRune(k)
 					z01.PrintRune(y)
 					z01.PrintRune(',')
-					z01.PrintRune(' ')
+					z01.PrintRune(' ')					
 				}
 			}
 		}
