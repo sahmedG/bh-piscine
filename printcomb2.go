b9a3ec8afd16d8df42d7cb468add1fbf28d54c42
reboot <sahmed>
2023-02-28 21:15:28 +0300

    second comb

--- printcomb2.go
+++ printcomb2.go
@@ -4,18 +4,18 @@ import "github.com/01-edu/z01"
 
 func PrintComb2() {
 	for i := '0'; i <= '9'; i++ {
-		for j := '0'; j < '9'; j++ {
+		for j := '0'; j <= '8'; j++ {
 			for k := '0'; k <= '9'; k++ {
-				for t:= '1'; t <= '9'; t++ {
+				for y := '1'; y <= '9'; y++ {
 					z01.PrintRune(i)
 					z01.PrintRune(j)
 					z01.PrintRune(' ')
 					z01.PrintRune(k)
-					z01.PrintRune(t)
+					z01.PrintRune(y)
 					z01.PrintRune(',')
 					z01.PrintRune(' ')
 				}
-			}				
+			}
 		}
 	}
 	z01.PrintRune('\n')
