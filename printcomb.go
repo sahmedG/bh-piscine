cebfd018dd394e602975d0f45cab67f05c521938
Sameer Ahmed <sahmed@noreply.learn.reboot01.com>
2023-02-28 09:40:21 +0000

    Update 'printcomb.go'

--- printcomb.go
+++ printcomb.go
@@ -1,7 +1,28 @@
 package piscine
 
-import "github.com/01-edu/z01"
+//import "github.com/01-edu/z01"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
-func PrintComb() {
-    z01.PrintRune()
+func main() {
+	var i int
+	var j int
+	var k int
+	array := make([]string, 0, 3)
+
+	for i = 0; i <= 9; i++ {
+		for j = i + 1; j <= 9; j++ {
+			for k = j + 1; k <= 9; k++ {
+				s1 := strconv.Itoa(i)
+				s2 := strconv.Itoa(j)
+				s3 := strconv.Itoa(k)
+				array = append(array, s1+s2+s3)
+			}
+		}
+	}
+	output := "" + strings.Join(array, `, `) + ``
+	fmt.Println(output)
 }
