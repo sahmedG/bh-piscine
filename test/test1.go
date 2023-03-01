92493c76d318ad339c7a60573466d1a82172355b
Reboot <engsameergoumaa@gmail.com>
2023-03-01 21:47:57 +0300

    fix

---
+++ test/test1.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	"piscine"
+)
+
+func main() {
+	fmt.Println(piscine.Atoi("12345"))
+	fmt.Println(piscine.Atoi("0000000012345"))
+	fmt.Println(piscine.Atoi("012 345"))
+	fmt.Println(piscine.Atoi("Hello World!"))
+	fmt.Println(piscine.Atoi("+1234"))
+	fmt.Println(piscine.Atoi("-1234"))
+	fmt.Println(piscine.Atoi("++1234"))
+	fmt.Println(piscine.Atoi("--1234"))
+	fmt.Println(piscine.Atoi("-8849638108820627584"))
+}
