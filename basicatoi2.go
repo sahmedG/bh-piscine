280a23d3f0c81564ff96df331bfc143bd5c11e37
Sameer Ahmed <sahmed@noreply.learn.reboot01.com>
2023-03-01 11:32:41 +0000

    Update 'basicatoi2.go'

--- basicatoi2.go
+++ basicatoi2.go
@@ -2,13 +2,18 @@ package piscine
 
 func BasicAtoi2(s string) int {
 	var res int
-
 	for i := 0; i < len(s); i++ {
 		if !(s[i] >= '0' && s[i] <= '9') {
 			return res
 		}
-		// Calculate the result for current iteration
-		res = res*10 + int(s[i]-'0')
+	}
+	byte_str := []rune(s)
+	for j := 0; j < len(byte_str); j++ {
+		if byte_str[j] == ' ' {
+			break
+		} else {
+			res = res*10 + int(byte_str[j]-'0')
+		}
 	}
 	return res
 }
