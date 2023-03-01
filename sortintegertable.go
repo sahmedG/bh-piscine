89c31de184eefc00715d1e38ac08da10dead3db6
Reboot <engsameergoumaa@gmail.com>
2023-03-01 22:14:09 +0300

    fix

---
+++ sortintegertable.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func SortIntegerTable(table []int) {
+	for i := 0; i < len(table); i++ {
+	}
+}
+
+func main() {
+	s := []int{5, 4, 3, 2, 1, 0}
+	SortIntegerTable(s)
+	fmt.Println(s)
+}
