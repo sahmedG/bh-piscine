7d2b796a07820e8bdc706442a84c3d1810f90c54
Sameer Ahmed <sahmed@noreply.learn.reboot01.com>
2023-03-01 11:18:43 +0000

    Update 'basicatoi2.go'

--- basicatoi2.go
+++ basicatoi2.go
@@ -1,16 +1,11 @@
 package piscine
 
-import "strings"
-
 func BasicAtoi2(s string) int {
 	var res int
 
 	for i := 0; i < len(s); i++ {
 		if !(s[i] >= '0' && s[i] <= '9') {
 			return res
-		}
-		if strings.Contains(s, " ") {
-			break
 		}
 		// Calculate the result for current iteration
 		res = res*10 + int(s[i]-'0')
