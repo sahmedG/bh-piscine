0bde65cd9e601aeb6da51266b7960e03a9d75bef
Sameer Ahmed <sahmed@noreply.learn.reboot01.com>
2023-03-01 01:35:41 +0000

    strrev

---
+++ strrev.go
@@ -0,0 +1,9 @@
+package piscine
+
+func StrRev(s string) string {
+	byte_str := []rune(s)
+	for i, j := 0, len(byte_str)-1; i < j; i, j = i+1, j-1 {
+		byte_str[i], byte_str[j] = byte_str[j], byte_str[i]
+	}
+	return string(byte_str)
+}
