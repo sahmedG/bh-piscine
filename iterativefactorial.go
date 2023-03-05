01255d7617cf5f74cbae6ba6215ad24375691552
Sameer Ahmed <sahmed@noreply.learn.reboot01.com>
2023-03-05 11:11:16 +0000

    Update 'iterativefactorial.go'

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -2,10 +2,10 @@ package piscine
 
 func IterativeFactorial(nb int) int {
 	Result := 1
-	if nb < 0 || nb > 12 {
+	if nb < 0 || nb > 26 {
 		Result = 0
 	}
-	if nb <= 12 {
+	if nb <= 26 {
 		for nb >= 1 {
 			Result *= nb
 			nb -= 1
