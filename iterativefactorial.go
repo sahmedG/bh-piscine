03e5dd3372e50ae89b7e49737b9a0b0cd24005c9
Sameer Ahmed <sahmed@noreply.learn.reboot01.com>
2023-03-05 08:45:37 +0000

    Update 'iterativefactorial.go'

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -4,9 +4,11 @@ func IterativeFactorial(nb int) int {
 	resutl := 1
 	if nb <= 0 {
 		return 0
-	}
-	for i := 1; i < nb+1; i++ {
-		resutl = resutl * i
+	} else {
+		for nb >= 1 {
+			resutl *= nb
+			nb -= 1
+		}
 	}
 	if resutl < 0 {
 		return 0
