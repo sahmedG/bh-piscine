e7f3630369a4cd368d44b52d5d833565b2f82942
Sameer Ahmed <sahmed@noreply.learn.reboot01.com>
2023-03-05 10:12:44 +0000

    Update 'iterativefactorial.go'

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -2,16 +2,14 @@ package piscine
 
 func IterativeFactorial(nb int) int {
 	resutl := 1
-	if nb <= 0 {
-		return 0
-	} else {
+	if nb > 0 {
 		for nb >= 1 {
-			resutl *= nb
-			nb -= 1
+		resutl *= nb
+		nb -= 1
+	    }
+		if resutl < 0 {
+			return 0
 		}
-	}
-	if resutl < 0 {
-		return 0
-	}
-	return resutl
-}
+		return resutl
+ }
+ 
