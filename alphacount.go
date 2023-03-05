3e3c95264b82c0b842014f187b6cefa523dbdde3
Sameer Ahmed <sahmed@noreply.learn.reboot01.com>
2023-03-05 11:30:02 +0000

    AlphaCount

--- alphacount.go
+++ alphacount.go
@@ -3,7 +3,7 @@ package piscine
 func AlphaCount(s string) int {
     count := 0
     for i := 0; i <= len(s); i++ {
-        if !(byte_str[start] >= 'a' && byte_str[start] <= 'z') {
+        if !(s[i] >= 'a' && s[i] <= 'z') {
             continue
         } else {
             count++
