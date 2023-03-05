4daf59add4db67b2e54d7802617d8c1f6f8233a1
Sameer Ahmed <sahmed@noreply.learn.reboot01.com>
2023-03-05 07:45:14 +0000

    nrune

---
+++ nrune.go
@@ -0,0 +1,16 @@
+package piscine
+
+func NRune(s string, n int) rune {
+	firstrune := []rune(s)
+	if n > len(firstrune) {
+		return 0
+	} else if n < 0 {
+		n = n * -1
+		last := firstrune[len(firstrune)-1]
+		return last
+	} else {
+		last := firstrune[n-1]
+		return last
+	}
+
+}
