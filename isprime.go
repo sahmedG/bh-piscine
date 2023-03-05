fa4eb7f79440d07add8b14bc3f13cef7694e6b28
reboot <student@desk-0120.reboot.internal>
2023-03-05 21:37:31 +0300

    fix

--- isprime.go
+++ isprime.go
@@ -1,15 +1,15 @@
 package piscine
 
 func IsPrime(nb int) bool {
-    primecount := 0
-    for i:= 2; i < nb; i++ {
-        if nb%i == 0 {
-            primecount++
-            break
-        }
-    }
-    if primecount == 0 && nb != 1 {
-        return true
-    }
-    return false
+	primecount := 0
+	for i := 2; i < nb; i++ {
+		if nb%i == 0 {
+			primecount++
+			break
+		}
+	}
+	if primecount == 0 && nb != 1 {
+		return true
+	}
+	return false
 }
