6809020871b042833035320c6a6ac25d303649d7
Sameer Ahmed <sahmed@noreply.learn.reboot01.com>
2023-03-05 08:20:46 +0000

    Update 'nrune.go'

--- nrune.go
+++ nrune.go
@@ -5,9 +5,7 @@ func NRune(s string, n int) rune {
 	if n > len(firstrune) || n == 0 {
 		return 0
 	} else if n < 0 {
-		n = n * -1
-		last := firstrune[len(firstrune)-n]
-		return last
+		return 0
 	} else {
 		last := firstrune[n-1]
 		return last
