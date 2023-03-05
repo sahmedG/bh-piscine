a574a13503b46dbfb6986429db62b7519bbfe10a
reboot <student@desk-0120.reboot.internal>
2023-03-05 21:51:22 +0300

    fix

---
+++ findnextprime.go
@@ -0,0 +1,18 @@
+package piscine
+
+func FindNextPrime(nb int) int {
+	primecount := 0
+	if nb <= 0 {
+		return 0
+	}
+	for i := 2; i < nb; i++ {
+		if nb%i == 0 {
+			primecount++
+			break
+		}
+	}
+	if primecount == 0 && nb != 1 {
+		return nb
+	}
+	return FindNextPrime(nb + primecount)
+}
