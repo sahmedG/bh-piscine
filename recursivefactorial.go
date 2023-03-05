2277c1849e8041c447b6365d071a768b49633ff2
Sameer Ahmed <sahmed@noreply.learn.reboot01.com>
2023-03-05 12:10:07 +0000

    Update 'recursivefactorial.go'

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -4,6 +4,8 @@ func RecursiveFactorial(nb int) int {
 	Result := 1
 	if nb < 0 || nb > 26 {
 		Result = 0
+	} else If nb == 0 {
+		Result = 0
 	}
 	Result = nb * RecursiveFactorial(nb-1)
 	return Result
