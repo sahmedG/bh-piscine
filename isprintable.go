5f8c70004f796c38257b93beffc8f1e40777b439
Sameer Ahmed <sahmed@noreply.learn.reboot01.com>
2023-03-06 09:43:39 +0000

    is printable

---
+++ isprintable.go
@@ -0,0 +1,11 @@
+package piscine
+
+func IsPrintable(s string) bool {
+	str := []rune(s)
+	for i := 0; i < len(s); i++ {
+		if str[i] == 9 || str[i] == 10 || str[i] == 11 || str[i] == 12 || str[i] == 13 || str[i] == 32 {
+			return false
+		}
+	}
+	return true
+}
