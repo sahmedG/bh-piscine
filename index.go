d3b24a0013698e3939b3e25ac41c773c109648de
Sameer Ahmed <sahmed@noreply.learn.reboot01.com>
2023-03-06 07:45:29 +0000

    Update 'index.go'

--- index.go
+++ index.go
@@ -1,9 +1,12 @@
 package piscine
 
 func Index(s string, toFind string) int {
-	l := len(s) - len(toFind)
+	l := len(s)
 	count := 0
 	i := 0
+	if len(s)-len(toFind) <= 1 {
+		l = len(s) - len(toFind)
+	}
 	for i = 0; i <= l; i++ {
 		for j := 0; j < len(toFind); j++ {
 			if s[i] == toFind[j] {
