274506a4b657044f91ee6fb4d9c32a4703331f81
Sameer Ahmed <sahmed@noreply.learn.reboot01.com>
2023-03-06 09:44:04 +0000

    fixing

--- isprintable.go
+++ isprintable.go
@@ -3,7 +3,7 @@ package piscine
 func IsPrintable(s string) bool {
 	str := []rune(s)
 	for i := 0; i < len(s); i++ {
-		if str[i] == 9 || str[i] == 10 || str[i] == 11 || str[i] == 12 || str[i] == 13 || str[i] == 32 {
+		if str[i] == 9 || str[i] == 10 || str[i] == 11 || str[i] == 12 || str[i] == 13 {
 			return false
 		}
 	}
