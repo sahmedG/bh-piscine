db29864d53fe4e4e8b7b53f7139cb14de57d5731
Sameer Ahmed <sahmed@noreply.learn.reboot01.com>
2023-03-06 10:33:05 +0000

    Update 'toupper.go'

--- toupper.go
+++ toupper.go
@@ -1,7 +1,12 @@
 package piscine
 
-import "strings"
-
-func ToUpper(s string) string {
-	return strings.ToUpper(s)
+func toUpper(str string) string {
+	var ret_str = ""
+	for _, chr := range str {
+		if chr >= 97 && chr <= 122 {
+			chr -= 32
+		}
+		ret_str += fmt.Sprintf("%c", chr)
+	}
+	return ret_str
 }
