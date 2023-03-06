2a00c7cb6337610904f7f1603f7ea84bb8d74be9
Sameer Ahmed <sahmed@noreply.learn.reboot01.com>
2023-03-06 08:13:49 +0000

    is alpha

---
+++ isalpha.go
@@ -0,0 +1,15 @@
+package piscine
+
+func IsAlpha(s string) bool {
+	count := 0
+	byte1 := []rune(s)
+	for i := 0; i < len(s); i++ {
+		if byte1[i] >= 'a' && byte1[i] <= 'z' || byte1[i] >= 'A' && byte1[i] <= 'Z' || byte1[i] >= '0' && byte1[i] <= '9' {
+			count++
+		}
+	}
+	if count == len(s) {
+		return true
+	}
+	return false
+}
