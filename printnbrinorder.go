5e79b43da2e26a340eb70d63fe31383d1908e69d
Your Name <you@example.com>
2023-03-06 20:08:25 +0300

    works

---
+++ printnbrinorder.go
@@ -0,0 +1,19 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintNbrInOrder(n int) {
+	numStr := ""
+	if n == 0 {
+		numStr = string(rune(n%10)) + numStr
+		n /= 10
+	}
+
+	for i := '0'; i <= '9'; i++ {
+		for _, char := range numStr {
+			if char == i {
+				z01.PrintRune(char)
+			}
+		}
+	}
+}
