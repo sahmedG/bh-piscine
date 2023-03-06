0f97183f2250b9e66a58b5025edf4c97dd9cbe12
Sameer Ahmed <sahmed@noreply.learn.reboot01.com>
2023-03-06 08:29:08 +0000

    Update 'isalpha.go'

--- isalpha.go
+++ isalpha.go
@@ -6,7 +6,7 @@ func IsAlpha(s string) bool {
 	}
 	count := 0
 	byte1 := []rune(s)
-	for i := len(s) - 1; i >= 0; i-- {
+	for i := 0; i < len(s); i++ {
 		if byte1[i] >= 'a' && byte1[i] <= 'z' || byte1[i] >= 'A' && byte1[i] <= 'Z' || byte1[i] >= '0' && byte1[i] <= '9' {
 			count++
 		}
