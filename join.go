764882e339da024e476b8b23ad52886f21dd04d9
Sameer Ahmed <sahmed@noreply.learn.reboot01.com>
2023-03-07 21:39:45 +0000

    join

---
+++ join.go
@@ -0,0 +1,14 @@
+package piscine
+
+func BasicJoin(elems []string) string {
+	words := ""
+	for i := 0; i < len(elems); i++ {
+		if i == len(elems)-1 {
+			words = words + elems[i]
+		} else {
+			words = words + elems[i] + ":"
+		}
+
+	}
+	return words
+}
