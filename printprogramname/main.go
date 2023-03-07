c4635138652d8898fbd29fdb0f5d6075046a8b8d
kalmahai <khalidalmehaiza@gmail.com>
2023-03-07 19:52:17 +0300

    name

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -1,10 +1,22 @@
 package main
 
 import (
-	"fmt"
 	"os"
+
+	"github.com/01-edu/z01"
 )
 
 func main() {
-	fmt.Println(os.Args[0])
+	argumet := os.Args[0]
+	d := 0
+	for i, letter := range argumet {
+		if letter == '/' {
+			d = i
+		}
+	}
+	name := []rune(argumet)[d+1:]
+	for _, i := range name {
+		z01.PrintRune(i)
+	}
+	z01.PrintRune('\n')
 }
