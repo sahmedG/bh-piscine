80fddb1a1402e59a5f2799d8fe8b2fcf306a1017
Sameer Ahmed <sahmed@noreply.learn.reboot01.com>
2023-03-07 20:26:47 +0000

    Update 'printwordstables.go'

--- printwordstables.go
+++ printwordstables.go
@@ -3,15 +3,15 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintWordsTables(a []string) {
-    for _,char := range(a) {
-        PrintStr(char)
-    }
+	for _, char := range a {
+		PrintStr(char)
+	}
 }
 
 func PrintStr(s string) {
-    str_rune := []rune(s)
-	for _, word := range s {
+	str_rune := []rune(s)
+	for _, word := range str_rune {
 		z01.PrintRune(word)
 	}
-    z01.PrintRune('\n')
+	z01.PrintRune('\n')
 }
