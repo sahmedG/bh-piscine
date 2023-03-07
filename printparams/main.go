e6320b9f84d98a03c58e0f8cf0fc546a3874ce34
kalmahai <khalidalmehaiza@gmail.com>
2023-03-07 20:15:31 +0300

    params

---
+++ printparams/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	argumet := os.Args
+	for i := 1; i <= len(argumet)-1; i++ {
+		name := []rune(argumet[i])
+		for i := 0; i <= len(name)-1; i++ {
+			z01.PrintRune(name[i])
+		}
+		z01.PrintRune('\n')
+	}
+}
