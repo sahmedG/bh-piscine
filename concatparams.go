73ae8abff58d6a4e36d8a69286eff14ded539b73
Sameer Ahmed <sahmed@noreply.learn.reboot01.com>
2023-03-07 08:02:43 +0000

    fixing

--- concatparams.go
+++ concatparams.go
@@ -2,10 +2,15 @@ package piscine
 
 func ConcatParams(args []string) string {
 	string1 := ""
+	var str_arr []string
 	if len(args) > 0 {
 		for i := 0; i <= len(args)-1; i++ {
-			string1 = string1 + args[i] + "\n"
+			str_arr = append(str_arr, string(args[i]))
+			str_arr = append(str_arr, "\n")
 		}
+	}
+	for j := 0; j < len(str_arr)-1; j++ {
+		string1 = string1 + str_arr[j]
 	}
 	return string1
 }
