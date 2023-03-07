9125fe4ded551a88d5cb5e5acba6b80d119c7ba1
Sameer Ahmed <sahmed@noreply.learn.reboot01.com>
2023-03-07 22:03:46 +0000

    Upload files to 'nbrconvertalpha'

---
+++ nbrconvertalpha/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	letters := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
+	argumet := os.Args
+	for i := 1; i <= len(argumet)-1; i++ {
+		name := []rune(argumet[i])
+		for t := 0; t < len(name); t++ {
+			j := name[t]
+			z01.Printrune(letters[j])
+		}
+	}
+}
