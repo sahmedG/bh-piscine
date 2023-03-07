b98f2188699d2a2f715cabefa6258ef3aecf6e3c
Sameer Ahmed <sahmed@noreply.learn.reboot01.com>
2023-03-07 21:48:33 +0000

    Update 'join.go'

--- join.go
+++ join.go
@@ -1,12 +1,12 @@
 package piscine
 
-func Join(elems []string) string {
+func Join(elems []string, sep string) string {
 	words := ""
 	for i := 0; i < len(elems); i++ {
 		if i == len(elems)-1 {
 			words = words + elems[i]
 		} else {
-			words = words + elems[i] + ":"
+			words = words + elems[i] + sep
 		}
 	}
 	return words
