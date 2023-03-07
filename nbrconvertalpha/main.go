071a3cbed83da4bf386af5618e75b64504cbd240
Sameer Ahmed <sahmed@noreply.learn.reboot01.com>
2023-03-07 22:36:41 +0000

    Update 'nbrconvertalpha/main.go'

--- nbrconvertalpha/main.go
+++ nbrconvertalpha/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	letters := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
+	letters := []rune{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}
 	argumet := os.Args
 	for i := 1; i <= len(argumet)-1; i++ {
 		name := []rune(argumet[i])
