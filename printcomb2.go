298d8f74ef40354c9761ac4c94a579969da9a8a1
Sameer Ahmed <sahmed@noreply.learn.reboot01.com>
2023-03-09 19:00:22 +0000

    Update 'printcomb2.go'

--- printcomb2.go
+++ printcomb2.go
@@ -3,29 +3,27 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb2() {
+
 	for i := '0'; i <= '9'; i++ {
 		for j := '0'; j <= '9'; j++ {
-			for k := '0'; k <= '9'; k++ {
-				for y := '0'; y <= '9'; y++ {
-					if i == '0' && j == '0' && k == '0' && y == '0' {
-						continue
-					} else if k+y <= i+j {
-						continue
-					} else {
-						z01.PrintRune(i)
-						z01.PrintRune(j)
-						z01.PrintRune(' ')
-						z01.PrintRune(k)
-						z01.PrintRune(y)
-						if i == '9' && j == '8' && k == '9' && y == '9' {
-							break
-						}
+			y := j + 1
+			for k := i; k <= '9'; k++ {
+				for ; y <= '9'; y++ {
+					z01.PrintRune((i))
+					z01.PrintRune((j))
+					z01.PrintRune(' ')
+					z01.PrintRune((k))
+					z01.PrintRune((y))
+					if i < '9' || j < '8' || k < '9' || y < '9' {
 						z01.PrintRune(',')
 						z01.PrintRune(' ')
 					}
+
 				}
+				y = '0'
 			}
 		}
 	}
 	z01.PrintRune('\n')
+
 }
