f5cce061bcca767806b373ddd325f438a9397ecd
Sameer Ahmed <sahmed@noreply.learn.reboot01.com>
2023-03-09 20:58:05 +0000

    Update 'rotatevowels/main.go'

--- rotatevowels/main.go
+++ rotatevowels/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"github.com/01-edu/z01"
 	"os"
+
+	"github.com/01-edu/z01"
 )
 
 func main() {
@@ -24,34 +25,30 @@ func main() {
 		runes := []rune(str)
 		var pos []int
 		var vow []rune
-
 		for i, r := range runes {
 			if r == 'A' || r == 'E' || r == 'I' || r == 'O' || r == 'U' || r == 'a' || r == 'e' || r == 'i' || r == 'o' || r == 'u' {
 				pos = append(pos, i)
 				vow = append(vow, runes[i])
 			}
 		}
-		swap (vow)
-
+		swap(vow)
 		for i := range pos {
-			runes[pos[i]] = vow [i]
+			runes[pos[i]] = vow[i]
 		}
-
 		for _, r := range runes {
 			z01.PrintRune(r)
 		}
 	}
 	z01.PrintRune('\n')
 }
-
-func swap (vow []rune) {
+func swap(vow []rune) {
 	len := 0
 	for i := range vow {
 		len = i + 1
 	}
-	for i, j := 0, len -1; i < j; i, j = i +1, j -1 {
+	for i, j := 0, len-1; i < j; i, j = i+1, j-1 {
 		temp := vow[i]
 		vow[i] = vow[j]
 		vow[j] = temp
-	} 
+	}
 }
