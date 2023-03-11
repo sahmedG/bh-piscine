d0b173147ce8e6a3e86fef04f91dc0fce773253b
Sameer Ahmed <sahmed@noreply.learn.reboot01.com>
2023-03-11 23:30:39 +0000

    Update 'point/main.go'

--- point/main.go
+++ point/main.go
@@ -18,25 +18,23 @@ func convertInttoRune(nbr int) {
 		count++
 	}
 	if nbr/10 != 0 {
-		convertInttoRune(nbr / 10)
+		convertInttoRune(nbr/10)
 	}
 	z01.PrintRune(count)
 }
 
 func main() {
+	xprint := []rune{'x', ' ', '=' ,' '}
 	points := &point{}
 	setPoint(points)
-	z01.PrintRune('x')
-	z01.PrintRune(' ')
-	z01.PrintRune('=')
-	z01.PrintRune(' ')
+	for _, value := range xprint {
+		z01.PrintRune(value)
+	}
 	convertInttoRune(points.x)
-	z01.PrintRune(',')
-	z01.PrintRune(' ')
-	z01.PrintRune('y')
-	z01.PrintRune(' ')
-	z01.PrintRune('=')
-	z01.PrintRune(' ')
+	yprint := []rune{',', ' ', 'y', ' ', '=', ' '}
+	for _, value := range yprint {
+		z01.PrintRune(value)
+	}
 	convertInttoRune(points.y)
 	z01.PrintRune('\n')
 }
