83ac2775e043d2f1bedf957e65109250d489df35
Sameer Ahmed <sahmed@noreply.learn.reboot01.com>
2023-03-11 22:06:19 +0000

    odd&even

---
+++ boolean/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func printStr(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
+	z01.PrintRune('\n')
+}
+
+func isEven(nbr int) bool {
+	if even(nbr) == 1 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func even(nbr int) int {
+	if nbr % 2 == 0{
+		return 1
+	} else {
+		return 0
+	}
+}
+
+func main() {
+	Args := os.Args[1:]
+	lenghtofArgs := len(Args)
+	EvenMsg := "I have an even number of arguments"
+	OddMsg := "I have an odd number of arguments"
+	if isEven(lenghtofArgs) == true {
+		printStr(EvenMsg)
+	} else {
+		printStr(OddMsg)
+	}
+}
