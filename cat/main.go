a01b6ea4d7d4ea6749ef9f541d1495ca9d9e5b7d
sahmed <engsameergoumaa@gmail.com>
2023-03-12 19:46:28 +0300

    another fix

--- cat/main.go
+++ cat/main.go
@@ -10,19 +10,20 @@ import (
 func main() {
 	args := os.Args[1:]
 	if len(args) < 1 {
-		z01.PrintRune('H')
-		z01.PrintRune('e')
-		z01.PrintRune('l')
-		z01.PrintRune('l')
-		z01.PrintRune('o')
-		z01.PrintRune('\n')
-		z01.PrintRune('H')
-		z01.PrintRune('e')
-		z01.PrintRune('l')
-		z01.PrintRune('l')
-		z01.PrintRune('o')
-		z01.PrintRune('\n')
+		input, err := ioutil.ReadAll(os.Stdin)
+		for _, char := range string(input) {
+			z01.PrintRune((char))
+		}
+
+		if err != nil {
+			for _, err := range err.Error() {
+				z01.PrintRune(err)
+			}
+			z01.PrintRune('\n')
+			os.Exit(1)
+		}
 	}
+
 	for i := 0; i < len(args); i++ {
 		data, err := ioutil.ReadFile(args[i])
 		if err != nil {
