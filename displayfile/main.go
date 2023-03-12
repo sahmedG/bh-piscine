467be5272c8f3ae407025e44351bb6b256da8624
Sameer Ahmed <sahmed@noreply.learn.reboot01.com>
2023-03-12 10:40:05 +0000

    Add 'displayfile/main.go'

---
+++ displayfile/main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+    filename := os.Ars[1:]
+    file, err := os.Open(filename)
+
+    if err != nil {
+        fmt.Printf("File name is missing : v%\n", err.Error())
+    }
+    arr := make([]byte)
+    file.Read(arr)
+    fmt.Println(string(arr))
+}
