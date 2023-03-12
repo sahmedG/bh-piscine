62d8452475a892cbcdcf2b0634bad4413e4fd15b
sahmed <engsameergoumaa@gmail.com>
2023-03-12 18:12:48 +0300

    fixing

--- displayfile/main.go
+++ displayfile/main.go
@@ -11,13 +11,13 @@ func main() {
 		fmt.Println("File name missing")
 	} else if len(args) > 1 {
 		fmt.Println("Too many arguments")
-	
-	} else { file, err := os.Open(args[0])
-	if err != nil {
-		fmt.Printf("Mistake: %v\n" , err.Error())
-	}
-	arr := make([]byte,20)
-	file.Read(arr)
-	fmt.Print(string(arr))
+	} else {
+		file, err := os.Open(args[0])
+		if err != nil {
+			fmt.Printf("Mistake: %v\n", err.Error())
+		}
+		arr := make([]byte, 20)
+		file.Read(arr)
+		fmt.Print(string(arr))
 	}
 }
