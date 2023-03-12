40adab47caa1939b3cd82f3579094fc9e7e85807
Sameer Ahmed <sahmed@noreply.learn.reboot01.com>
2023-03-12 20:05:00 +0000

    any

---
+++ any.go
@@ -0,0 +1,12 @@
+package main
+
+func Any(f func(string) bool, a []string) bool {
+	var result bool
+	for i := range a {
+		result = f(a[i])
+		if result == true {
+			return result
+		}
+	}
+	return result
+}
