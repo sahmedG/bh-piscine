240fe04c7ff715a50f27ab12e22c76d59f388b51
Sameer Ahmed <sahmed@noreply.learn.reboot01.com>
2023-03-12 11:09:54 +0000

    Update 'displayfile/main.go'

--- displayfile/main.go
+++ displayfile/main.go
@@ -5,7 +5,6 @@ import "fmt"
 func main() {
     filename := os.Ars[1:]
     file, err := os.Open(filename)
-
     if err != nil {
         fmt.Printf("File name is missing : v%\n", err.Error())
     }
