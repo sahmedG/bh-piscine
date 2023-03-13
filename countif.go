38a73f9248854742594a79ac77bf16550c0c20e0
Sameer Ahmed <sahmed@noreply.learn.reboot01.com>
2023-03-13 05:13:33 +0000

    countifs

---
+++ countif.go
@@ -0,0 +1,16 @@
+package piscine
+
+func CountIf(f func(string) bool, a []string) int {
+	l := len(a)
+	count := 0
+	result := make([]bool, l)
+	for i := range a {
+		result[i] = f(a[i])
+	}
+	for j:=0;j<len(result);j++ {
+		if result[j] == true {
+			count += 1
+		}
+	}
+	return count
+}
