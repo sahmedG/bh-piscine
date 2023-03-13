c8ec59e9f1e3b1e923569a63885bbf3236551a01
Sameer Ahmed <sahmed@noreply.learn.reboot01.com>
2023-03-13 05:42:28 +0000

    Update 'issorted.go'

--- issorted.go
+++ issorted.go
@@ -16,11 +16,11 @@ func IsSorted(f func(a, b int) int, a []int) bool {
 }
 
 func f(a, b int) int {
-	if a > b {
-		return -1
+	if b > a {
+		return 1
 	} else if a == b {
 		return 0
 	} else {
-		return 1
+		return -1
 	}
 }
