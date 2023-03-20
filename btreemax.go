b232ad518937441a2a288e41db0b55ea6676a836
sahmed <engsameergoumaa@gmail.com>
2023-03-20 12:09:03 +0300

    test

---
+++ btreemax.go
@@ -0,0 +1,16 @@
+package piscine
+
+func BTreeMax(root *TreeNode) *TreeNode {
+	if root == nil {
+		return root
+	}
+	res := root
+	lres := BTreeMax(root.Left)
+	rres := BTreeMax(root.Right)
+	if lres.Data > res.Data {
+		res = lres
+	} else if rres.Data > res.Data {
+		res = rres
+	}
+	return res
+}
