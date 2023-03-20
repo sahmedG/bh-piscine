ad4a3123f9f0dc0bb5765f25ffd51554bd93f3f8
sahmed <engsameergoumaa@gmail.com>
2023-03-20 09:19:30 +0300

    should work

---
+++ listfind.go
@@ -0,0 +1,26 @@
+package piscine
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func CompStr(a, b interface{}) bool {
+	return a == b
+}
+
+func ListFind(l *List, ref interface{}, comp func(a, b interface{}) bool) *interface{} {
+	a := l.Head
+	for a != nil {
+		if comp(ref, a.Data) == true {
+			return &a.Data
+		}
+		a = a.Next
+	}
+	return nil
+}
