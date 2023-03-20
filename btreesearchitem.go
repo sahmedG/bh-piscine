df331233633403f5de4a80c738fc75584463ac54
sahmed <engsameergoumaa@gmail.com>
2023-03-20 10:55:53 +0300

    fix

--- btreesearchitem.go
+++ btreesearchitem.go
@@ -1,10 +1,5 @@
 package piscine
 
-type TreeNode struct {
-	Left, Right, Parent *TreeNode
-	Data                string
-}
-
 func BTreeSearchItem(root *TreeNode, elem string) *TreeNode {
 	if root == nil || root.Data == elem {
 		return root
@@ -13,18 +8,4 @@ func BTreeSearchItem(root *TreeNode, elem string) *TreeNode {
 		return BTreeSearchItem(root.Left, elem)
 	}
 	return BTreeSearchItem(root.Right, elem)
-}
-
-func BTreeInsertData(root *TreeNode, data string) *TreeNode {
-	if root == nil {
-		return &TreeNode{Data: data}
-	}
-	if data < root.Data {
-		root.Left = BTreeInsertData(root.Left, data)
-		root.Left.Parent = root
-	} else if data > root.Data {
-		root.Right = BTreeInsertData(root.Right, data)
-		root.Right.Parent = root
-	}
-	return root
 }
