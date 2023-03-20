78c6fc564204049f790cc9cecad2632214a2d456
sahmed <engsameergoumaa@gmail.com>
2023-03-20 10:11:49 +0300

    test

--- sortlistinsert.go
+++ sortlistinsert.go
@@ -1,7 +1,7 @@
 package piscine
 
 func SortListInsert(l *NodeI, data_ref int) *NodeI {
-	l = ListPushBack(l, data_ref)
-	l = ListSort(l)
+	*l = ListPushBack(l, data_ref)
+	*l = ListSort(l)
 	return l
 }
