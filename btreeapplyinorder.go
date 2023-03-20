a2acd572ba235bdc882379edd97748f8f3ede5d4
sahmed <engsameergoumaa@gmail.com>
2023-03-20 09:40:48 +0300

    btreeorder

--- btreeapplyinorder.go
+++ btreeapplyinorder.go
@@ -3,7 +3,7 @@ package piscine
 func BTreeApplyInorder(root *TreeNode, f func(...interface{}) (int, error)) {
 	if root != nil {
 		f(root.Data)
-		BTreeApplyInorder(root.Left, f)
 		BTreeApplyInorder(root.Right, f)
+		BTreeApplyInorder(root.Left, f)
 	}
 }
