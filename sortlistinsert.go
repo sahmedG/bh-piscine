c579fabc0df53ba7308fa3502e9f9a1a3fa6e7dc
sahmed <engsameergoumaa@gmail.com>
2023-03-20 10:26:23 +0300

    fixed

--- sortlistinsert.go
+++ sortlistinsert.go
@@ -1,7 +1,20 @@
 package piscine
 
 func SortListInsert(l *NodeI, data_ref int) *NodeI {
-	*l = ListPushBack(l, data_ref)
-	*l = ListSort(l)
+	l = ListPushBackNode(l, data_ref)
+	l = ListSort(l)
+	return l
+}
+
+func ListPushBackNode(l *NodeI, data int) *NodeI {
+	n := &NodeI{Data: data, Next: nil}
+	if l == nil {
+		return n
+	}
+	a := l
+	for a.Next != nil {
+		a = a.Next
+	}
+	a.Next = n
 	return l
 }
