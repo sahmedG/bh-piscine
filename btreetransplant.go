7b03caa6d93646080b599a0fcb391dc043129080
sahmed <engsameergoumaa@gmail.com>
2023-03-20 13:21:24 +0300

    transplant

--- btreetransplant.go
+++ btreetransplant.go
@@ -5,9 +5,7 @@ func BTreeTransplant(root, node, rplc *TreeNode) *TreeNode {
 		return nil
 	}
 	replacement := node
-	if node.Parent == nil {
-		root = rplc
-	} else if node == node.Parent.Right {
+	if node == node.Parent.Right {
 		replacement.Parent.Right = rplc
 	}
 	replacement.Parent = node.Parent
