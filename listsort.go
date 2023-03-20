539905cd09aa9727cf613b6a89a9bf02d5b0be2f
sahmed <engsameergoumaa@gmail.com>
2023-03-20 09:50:45 +0300

    sortting

---
+++ listsort.go
@@ -0,0 +1,26 @@
+package piscine
+
+func ListSort(l *NodeI) *NodeI {
+	Head := l
+	if Head == nil {
+		return nil
+	}
+	Head.Next = ListSort(Head.Next)
+	if Head.Next != nil && Head.Data > Head.Next.Data {
+		Head = exchange(Head)
+	}
+	return Head
+}
+
+func exchange(l *NodeI) *NodeI {
+	a := l
+	b := l.Next
+	c := b
+	for b != nil && l.Data > b.Data {
+		a = b
+		b = b.Next
+	}
+	a.Next = l
+	l.Next = b
+	return c
+}
